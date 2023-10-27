749ebf131e7d6db4a0cc9797bcab55dc56720250
MrAzimzadeh <mehemmedezimzase@gmail.com>
2023-10-27 12:35:55 +0400

    Create New Project (Client And Server connection example ) & start new Custom gRPC Service

---
+++ grpcClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using Grpc.Net.Client;
+using grpcServer;
+
+Console.WriteLine("Clienr");
+
+
+var chanel = GrpcChannel.ForAddress("https://localhost:7245");
+var greetClient = new Greeter.GreeterClient(channel: chanel);
+
+HelloReply result = await greetClient.SayHelloAsync(new HelloRequest
+{
+    Name = "Salam Dostlarrr "
+});
+Console.WriteLine(result.Message);
